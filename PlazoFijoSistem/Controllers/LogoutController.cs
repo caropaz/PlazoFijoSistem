2b926b599f6eec807e97c295e4196533c4054e16
Caro Paz <pazcaro979@gmail.com>
2022-06-20 16:44:19 -0300

    terminado

---
+++ PlazoFijoSistem/Controllers/LogoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlazoFijoSistem.Controllers
+{
+    [Authorize(Roles = "ADMIN,USER")]
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+            
+        }
+    }
+}
